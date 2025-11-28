eda8b2fd91f2bd2e44e4b6b031f1ee119e2e1569
bmcho <bmcho91@naver.com>
2025-11-28 17:12:53 +0900

    fix: resolve Kafka connection error by updating server bootstrap address to localhost

--- coupon-service/src/main/java/com/bmcho/couponservice/config/interceptor/KafkaConfig.java
+++ coupon-service/src/main/java/com/bmcho/couponservice/config/interceptor/KafkaConfig.java
@@ -17,7 +17,7 @@ import java.util.Map;
 
 @Configuration
 public class KafkaConfig {
-    private static final String BOOTSTRAP_SERVERS = "kafka:9092";
+    private static final String BOOTSTRAP_SERVERS = "localhost:9092";
     private static final String GROUP_ID = "coupon-service";
 
     @Bean
