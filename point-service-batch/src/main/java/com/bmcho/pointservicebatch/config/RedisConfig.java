91988c8856f8e334234b5743b1c6856e51572f7e
bmcho <bmcho91@naver.com>
2025-12-17 17:18:05 +0900

    fix(batch): fixed redis configuration

--- point-service-batch/src/main/java/com/bmcho/pointservicebatch/config/RedisConfig.java
+++ point-service-batch/src/main/java/com/bmcho/pointservicebatch/config/RedisConfig.java
@@ -2,15 +2,26 @@ package com.bmcho.pointservicebatch.config;
 
 import org.redisson.Redisson;
 import org.redisson.api.RedissonClient;
+import org.redisson.config.Config;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class RedisConfig {
 
+    @Value("${spring.data.redis.host}")
+    private String host;
+    @Value("${spring.data.redis.port}")
+    private String port;
+
     @Bean(destroyMethod = "shutdown")
     public RedissonClient redissonClient() {
-        return Redisson.create(); //기본값 localhost, 6379
+        String address = "redis://" + host + ":" + port;
+        Config config = new Config();
+        config.useSingleServer()
+                .setAddress(address);
+        return Redisson.create();
     }
 
 }
