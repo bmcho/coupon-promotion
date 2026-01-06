6812dc66313a3719e7759b56bf96a80a489c2d48
bmcho <bmcho91@naver.com>
2026-01-06 16:26:18 +0900

    fix(time-sale): add EnableDiscoveryClient Annotation for Eureka Server

--- time-sale-service/src/main/java/com/bmcho/timesaleservice/TimeSaleServiceApplication.java
+++ time-sale-service/src/main/java/com/bmcho/timesaleservice/TimeSaleServiceApplication.java
@@ -2,8 +2,10 @@ package com.bmcho.timesaleservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class TimeSaleServiceApplication {
 
     public static void main(String[] args) {
